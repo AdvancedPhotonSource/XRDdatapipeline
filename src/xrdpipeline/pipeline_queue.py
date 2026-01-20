76b1aa4c9f5a87f742f69efd0a8c5340868fbefb
Anna Henckel Merritt <amerritt@anl.gov>
2026-01-20 15:46:29 -0600

    Add command line options to run pipeline UI with prefilled information

--- src/xrdpipeline/pipeline_queue.py
+++ src/xrdpipeline/pipeline_queue.py
@@ -533,7 +533,7 @@ class AdvancedSettings(QtWidgets.QWidget):
         self.calc_spottiness_types = [
             "None",
             "Spot Area Stats Only",
-            "Gradient Statistics",
+            "Spot Area Stats and Gradient Statistics",
         ]
         self.calc_spottiness_combobox.addItems(self.calc_spottiness_types)
         self.calc_spottiness_default = 1
@@ -595,7 +595,27 @@ class main_window(QtWidgets.QWidget):
     """
     Main UI window.
     """
-    def __init__(self, input_directory=None, output_directory=None, imctrl=None, flatfield=None, imgmask=None, bad_pixels=None):
+    def __init__(
+            self,
+            input_directory=None,
+            output_directory=None,
+            imctrl=None,
+            flatfield=None,
+            imgmask=None,
+            bad_pixels=None,
+            tth_integration_range=None,
+            azim_integration_range=None,
+            n_integration_bins=None,
+            polarization=None,
+            csim_first_index=None,
+            outlier_mad_mult=None,
+            n_mask_bins=None,
+            azim_Q_ratio=None,
+            outlier_option=None,
+            spottiness_option=None,
+            files_must_include=None,
+            files_must_exclude=None,
+        ):
         super().__init__()
         # Set up logging
         logging.getLogger('').setLevel(logging.INFO)
@@ -708,6 +728,62 @@ class main_window(QtWidgets.QWidget):
         self.settings = {}
         self.settings_widget = AdvancedSettings(settings=self.settings)
         self.settings_shown = False
+
+        # Update imctrl data if a file was passed, then update any passed override settings
+        if imctrl is not None:
+            self.update_imctrl_data()
+        if tth_integration_range is not None:
+            current = [self.iotth_min.value(), self.iotth_max.value()]
+            try:
+                self.iotth_min.setValue(tth_integration_range[0])
+                self.iotth_max.setValue(tth_integration_range[1])
+            except:
+                self.iotth_min.setValue(current[0])
+                self.iotth_max.setValue(current[1])
+        if azim_integration_range is not None:
+            current = [self.azim_min.value(), self.azim_max.value()]
+            try:
+                self.azim_min.setValue(azim_integration_range[0])
+                self.azim_max.setValue(azim_integration_range[1])
+            except:
+                self.azim_min.setValue(current[0])
+                self.azim_min.setValue(current[1])
+        if n_integration_bins is not None:
+            self.outChannels.setValue(n_integration_bins)
+        if polarization is not None:
+            self.PolaVal.setValue(polarization)
+        if csim_first_index is not None:
+            self.settings_widget.csim_first_spinbox.setValue(csim_first_index)
+        if outlier_mad_mult is not None:
+            self.settings_widget.madmult.setValue(outlier_mad_mult)
+            self.settings_widget.madmult_override.setChecked(True)
+        if n_mask_bins is not None:
+            self.settings_widget.nbins_om.setValue(n_mask_bins)
+            self.settings_widget.nbins_om_override.setChecked(True)
+        if azim_Q_ratio is not None:
+            self.settings_widget.azim_q.setValue(azim_Q_ratio)
+            self.settings_widget.azim_q_override.setChecked(True)
+        if outlier_option is not None:
+            if outlier_option == "splitting":
+                self.settings_widget.calc_outlier_checkbox.setChecked(True)
+                self.settings_widget.calc_splitting_checkbox.setChecked(True)
+            elif outlier_option == "outlier_only":
+                self.settings_widget.calc_outlier_checkbox.setChecked(True)
+                self.settings_widget.calc_splitting_checkbox.setChecked(False)
+            elif outlier_option == "none":
+                self.settings_widget.calc_outlier_checkbox.setChecked(False)
+                self.settings_widget.calc_splitting_checkbox.setChecked(False)
+        if spottiness_option is not None:
+            if spottiness_option == "none":
+                self.settings_widget.calc_spottiness_combobox.setCurrentIndex(0)
+            elif spottiness_option == "spot_area_only":
+                self.settings_widget.calc_spottiness_combobox.setCurrentIndex(1)
+            elif spottiness_option == "spot_and_gradient":
+                self.settings_widget.calc_spottiness_combobox.setCurrentIndex(2)
+        if files_must_include is not None:
+            self.settings_widget.regex_include_text.setText(files_must_include)
+        if files_must_exclude is not None:
+            self.settings_widget.regex_exclude_text.setText(files_must_exclude)
 
         # self.time_checkpoints = ["Start","Image loaded","Cache","Zero mask","Polar-correct","Outlier mask","Closing mask","Split first mask","Split second mask","All integrations","Save integrals","Delete project"]
         # self.time_checkpoints = ["Start", "Cache", "Zero mask", "Outlier mask", "Closing mask", "Splitting mask", "Integrations", "Save integrals", "CSim", "NMI", "SSim"]
@@ -1415,15 +1491,29 @@ class main_window(QtWidgets.QWidget):
             evt.ignore()
 
 
-# Following https://gsas-ii.readthedocs.io/en/latest/GSASIIscriptable.html 16.7.9. Optimized Image Integration
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input_directory")
-    parser.add_argument("-o", "--output_directory")
-    parser.add_argument("-c", "--imctrl")
-    parser.add_argument("-f", "--flatfield", default=None)
-    parser.add_argument("-m", "--imgmask", default=None)
-    parser.add_argument("-b", "--bad_pixels", default=None)
+    parser.add_argument("-i", "--input_directory", help="Location of the input image directory")
+    parser.add_argument("-o", "--output_directory", help="Location to place the output files from this pipeline")
+    parser.add_argument("-c", "--imctrl", help="Image control file")
+    parser.add_argument("-f", "--flatfield", help="Flatfield file")
+    parser.add_argument("-m", "--imgmask", help="Experimental mask")
+    parser.add_argument("-b", "--bad_pixels", help="Detector known bad pixel mask")
+    parser.add_argument("-t", "--tth_integration_range", nargs=2, type=float, help= "2theta integration range, if overriding or not included in the image control file. Provide minimum and maximum values separated by a space.")
+    parser.add_argument("-z", "--azim_integration_range", nargs=2, type=float, help="Azimuthal integration range, if overriding or not included in the image control file. Provide minimum and maximum values separated by a space.")
+    parser.add_argument("--n_integration_bins", type=float, help="Number of bins to use for integration, if overriding or not included in the config file.")
+    parser.add_argument("-p", "--polarization", type=float, help="Polarization of the image")
+    parser.add_argument("--csim_first_index", help="Numerical index for the file which should be considered first when calculating cosine similarity.")
+    # parser.add_argument("--outlier_mad_mult", type=float, default=3, help="Multiplier of median absolute deviation to use when considering a value an outlier.")
+    parser.add_argument("--outlier_mad_mult", type=float, help="Multiplier of median absolute deviation to use when considering a value an outlier. Default is 3.")
+    parser.add_argument("--n_mask_bins", type=int, help="Number of bins used when calculating outliers.")
+    # parser.add_argument("-a", "--azim_Q_ratio", type=int, default=100, help="Azimuthal to Q width ratio used for classifying spots.")
+    parser.add_argument("-a", "--azim_Q_ratio", type=int, help="Azimuthal to Q width ratio used for classifying spots. Default is 100.")
+    parser.add_argument("--outlier_option", choices=["splitting", "outlier_only", "none"], default="splitting", help="Choose whether to perform no outlier masking, outlier masking only, or outlier masking with spot/texture splitting.")
+    parser.add_argument("--spottiness_option", choices=["spot_and_gradient","spot_area_only","none"], default="spot_area_only", help="Choose whether to perform spottiness statistics calculations.")
+    parser.add_argument("--files_must_include", help="Process only files in the directory which include the provided string in their name.")
+    parser.add_argument("--files_must_exclude", help="Exclude files in the directory which have the provided string in their name.")
+    # parser.add_argument("-n", "--no_ui", action="store_true", help="Skip the UI and run over files with the specified options (otherwise options will be pre-filled in the UI). Must include input directory, output directory, and image control file to run.")
     args = parser.parse_args()
 
     # Pass in location and names of files
@@ -1468,5 +1558,24 @@ if __name__ == "__main__":
         imgctrl = None
 
     app = QtWidgets.QApplication([])
-    window = main_window(input_directory=input_directory, output_directory=output_directory, imctrl=imgctrl, flatfield=flatfield, imgmask=imgmask, bad_pixels=bad_pixels)
+    window = main_window(
+        input_directory=input_directory,
+        output_directory=output_directory,
+        imctrl=imgctrl,
+        flatfield=flatfield,
+        imgmask=imgmask,
+        bad_pixels=bad_pixels,
+        tth_integration_range=args.tth_integration_range,
+        azim_integration_range=args.azim_integration_range,
+        n_integration_bins=args.n_integration_bins,
+        polarization=args.polarization,
+        csim_first_index=args.csim_first_index,
+        outlier_mad_mult=args.outlier_mad_mult,
+        n_mask_bins=args.n_mask_bins,
+        azim_Q_ratio=args.azim_Q_ratio,
+        outlier_option=args.outlier_option,
+        spottiness_option=args.spottiness_option,
+        files_must_include=args.files_must_include,
+        files_must_exclude=args.files_must_exclude,
+    )
     sys.exit(app.exec())
